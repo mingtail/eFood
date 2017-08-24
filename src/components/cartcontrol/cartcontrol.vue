d30edab47b557bd8c1ad92a156ccb1e02d6f8fc1
mingtail <mingtail@qq.com>
2017-08-24 14:32:10 +0800

    add new

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="cart-control">
-    <div v-show="food.count>0" class="cart-decrease">
-      <i class="icon-remove_circle_outline"></i>
-    </div>
+    <transition name="move">
+      <div v-show="food.count>0" class="cart-decrease" @click="decreaseFood">
+        <i class="inner icon-remove_circle_outline"></i>
+      </div>
+    </transition>
     <div v-show="food.count>0" class="cart-count">{{food.count}}</div>
     <div class="cart-add" @click="addFood">
       <i class="icon-add_circle"></i>
@@ -17,12 +19,20 @@
     },
     methods: {
       addFood () {
-        // console.log('clicked')
+//        if (!ev._constructed) {
+//          return
+//        }
+        console.log(111)
         if (!this.food.count) {
           console.log(this)
           this.$set(this.food, 'count', 1)
         } else {
           this.food.count++
+        }
+      },
+      decreaseFood () {
+        if (this.food.count) {
+          this.food.count--
         }
       }
     }
@@ -30,17 +40,51 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+
+  .move-enter-active, .move-leave-active {
+    transition all .4s linear
+    .inner {
+      transform rotate(0)
+    }
+  }
+  .move-enter, .move-leave-to {
+    transform translate3D(48px, 0, 0)
+    opacity 0
+    .inner {
+      transform rotate(180deg)
+    }
+  }
+
   .cart-control {
     font-size 0
-    .cart-decrease, .cart-add {
+    .cart-decrease {
+      display inline-block
+      padding 6px
+      .inner {
+        display inline-block
+        line-height 24px
+        font-size 24px
+        color rgb(0, 160, 220)
+        transition all .4s linear
+      }
+
+    }
+    .cart-count {
+      display inline-block
+      padding-top 6px
+      vertical-align top
+      width 12px
+      line-height 24px
+      text-align center
+      font-size 10px
+      color rgb(147, 153, 159)
+    }
+    .cart-add {
       display inline-block
       padding 6px
       line-height 24px
       font-size 24px
       color rgb(0, 160, 220)
-    }
-    .cart-count {
-      display inline-block
     }
   }
 </style>
