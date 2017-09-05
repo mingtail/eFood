d590e8ae9c6c19b8142308c6a12e2f425aae511b
mingtail <mingtail@qq.com>
2017-09-05 16:09:01 +0800

    add new

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,22 +1,56 @@
 <template>
-  <div class="food" v-show="showFlag">
+  <transition name="move">
+    <div class="food" v-show="showFlag" ref="food">
+      <div class="food-content">
+        <div class="img-header">
+          <img :src="food.image">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div>
 
-  </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now">¥ {{food.price}}</span><span v-show="food.oldPrice" class="old">¥ {{food.oldPrice}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script type="text/javascript">
+  import BScroll from 'better-scroll'
+
   export default {
     props: {
       food: Object
     },
-    data() {
+    data () {
       return {
         showFlag: false
       }
     },
     methods: {
-      show() {
+      show () {
         this.showFlag = true
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        })
+      },
+      hide () {
+        this.showFlag = false
       }
     }
   }
@@ -32,5 +66,72 @@
     width 100%
     z-index 30
     background-color #fff
+    &.move-enter-active, &.move-leave-active {
+      transition all .2s linear
+    }
+    &.move-enter, &.move-leave-to {
+      transform translate3d(100%, 0, 0)
+    }
+    .img-header {
+      position relative
+      width 100%
+      height 0
+      padding-top 100%
+      & > img {
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+      }
+      .back {
+        position absolute
+        top 10px
+        left 0
+        .icon-arrow_lift {
+          display block
+          padding 10px
+          font-size 20px
+          color #fff
+        }
+      }
+    }
+    .content {
+      padding 18px
+      .title {
+        margin-bottom 8px
+        line-height 14px
+        font-size 14px
+        font-weight 700
+        color rgb(7, 17, 27)
+      }
+      .detail {
+        margin-bottom 18px
+        height 10px
+        line-height 10px
+        font-size 0
+        .sell-count, .rating {
+          font-size 10px
+          color rgb(147, 153, 159)
+        }
+        .sell-count {
+          margin-right 12px
+        }
+      }
+      .price {
+        font-weight 700
+        line-height 24px
+        .now {
+          margin-right 8px
+          font-size 14px
+          color rgb(240, 20, 20)
+        }
+        .old {
+          text-decoration line-through
+          font-size 10px
+          color rgb(147, 153, 159)
+        }
+      }
+    }
   }
 </style>
